3e8aabd4fd495804d282502ada0cfa133951cf1d
Lemio <kota.shimizu.biz@gmail.com>
2023-09-28 13:55:03 +0900

    カレンダー登録のみの機能を追加

--- llm.py
+++ llm.py
@@ -181,3 +181,130 @@ def extract_event_details(input_event_data):
     
     return place_response, times_response, event_response
 
+def calendar_registration(input_event_data):
+
+    current_datetime = datetime.now()
+
+    times_prompt = f'''
+    Overview:
+    Extract Google Calendar event details from the text provided and extract the event place.
+
+    Steps:
+
+    Extract event details from the text.
+
+
+    Parameters:
+
+    dates: Event start times. 
+    If the year, month, etc. cannot be confirmed, {current_datetime} is used as the current time for reference.
+
+    Output:
+    Output the extracted event start time in the format "YYYYY,MM,DD,HH,MM,SS".
+
+    Example of output format:
+    Output is for September 28, 2023 at 19:00
+    2023, 9, 28, 19, 00, 00
+
+
+    Input:
+    {input_event_data}
+    '''
+
+    # モデルを呼び出し、レスポンスを取得
+    times_response = openai.Completion.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=times_prompt,
+        temperature=0,
+        max_tokens=1000
+    )
+
+    # レスポンスからテキスト部分を取り出す
+    times_response = times_response.choices[0].text.strip()
+
+    if times_response.startswith("Output:\n\n"):
+        times_response = times_response[len("Output:\n\n"):]
+
+    if times_response.startswith("Output:\n"):
+        times_response = times_response[len("Output:\n"):]
+
+    # times_responseをdatetimeオブジェクトに変換
+    year, month, day, hour, minute, second = map(int, times_response.split(','))
+    times_response = datetime(year, month, day, hour, minute, second)
+
+    # UTCに変換（日本時間はUTC+9）
+    utc_start_time = times_response - timedelta(hours=9)
+    utc_end_time = utc_start_time + timedelta(hours=2)
+
+    # 新しい形式に変換
+    jp_start_time_str = utc_start_time.strftime("%Y%m%dT%H%M%SZ")
+    jp_end_time_str = utc_end_time.strftime("%Y%m%dT%H%M%SZ")
+    Sample_Scheduled_time = jp_start_time_str + "/" + jp_end_time_str
+    
+    
+    event_prompt = f'''
+    Google Calendar URL Creation Template
+
+    Overview:
+    Guide to create a Google Calendar event URL from provided text. The aim is to extract and convert event details into a structured URL format, avoiding redundancy and ensuring clarity.
+
+    Steps:
+
+    Extract event details from the text.
+
+    Retrieve location or meeting place details.
+    Prioritize precise names of places or addresses for the location field to ensure compatibility with Google Maps.
+    If precise location details aren't available, use descriptive location details from the text.
+
+    Synthesize a title:
+
+    Generate a comprehensive title based on the input content and extracted information. Avoid including the location in the title to prevent redundancy.
+
+    Convert details to Google Calendar URL parameters:
+
+    Default timezone is Asia/Tokyo unless specified.
+    The start time of the event is determined by {times_response} and is expressed as {jp_start_time_str} considering the time zone.
+    If the end time isn't provided, make an educated guess based on the event type (e.g., for lunch, it is 2 hours and is represented as {Sample_Scheduled_time}).
+    
+    Parameters:
+
+    text: Generated event title that's comprehensive and non-redundant.
+    dates: Event start and end times. Convert Asia/Tokyo time to UTC, considering a 9-hour difference unless otherwise mentioned.
+    details: Event details link, which can be web page URLs or Zoom links. Reconstruct the details in a coherent manner, considering the intent and context of the provided information.
+    location: Precise name of the place or address, ensuring compatibility with Google Maps. Use descriptive details only if precise details aren't available.
+    trp: Set to false.
+    Output:
+    Google Calendar event URL matching the input text language.
+
+    Input:
+    {input_event_data}    '''
+    
+    
+    
+    # print(times_response)
+    
+        # モデルを呼び出し、レスポンスを取得
+    event_response = openai.Completion.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=event_prompt,
+        temperature=0,
+        max_tokens=1500
+    )
+    
+    event_response = event_response.choices[0].text.strip()
+    
+    if event_response.startswith("Output:\n\n"):
+        event_response = event_response[len("Output:\n\n"):]
+
+    if event_response.startswith("Output:\n"):
+        event_response = event_response[len("Output:\n"):]
+
+    if event_response.startswith("Output:"):
+        event_response = event_response[len("Output:\n"):]
+        
+    if ' ' in event_response or '　' in event_response:
+        event_response = re.sub(r'[ 　]', '', event_response)
+    else:
+        event_response = event_response
+    
+    return event_response
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import googlemaps
 from datetime import datetime, timedelta, timezone
 import streamlit as st
-from llm import extract_event_details
+from llm import extract_event_details, calendar_registration
 # import pandas as pd
 
 
@@ -24,48 +24,61 @@ event_info = st.text_area("予定情報:", placeholder="スケジュール内容
 # st.caption("スケジュール内容をコピペ → 予定名：全日本模型ホビーショー\n場所：東京ビッグサイト 南1・2ホール\n日時：2023年9月30日 14:00")
 # st.caption("または日付けと目的地が入った文章を入力 → 10/15 18時から渋谷集合で")
 
-if st.button("URL生成"):
-    with st.spinner('生成処理中...'):
-        place_response, times_response, event_response = extract_event_details(event_info)  # 関数を呼び出し
-        arrival_point = place_response  # 到着地点にplace_responseを格納
+# 2つのカラムを作成
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("URL生成"):
+        with st.spinner('生成処理中...'):
+            place_response, times_response, event_response = extract_event_details(event_info)  # 関数を呼び出し
+            arrival_point = place_response  # 到着地点にplace_responseを格納
+            
+            # times_responseをdatetimeオブジェクトに変換
+            # year, month, day, hour, minute, second = map(int, times_response.split(','))
+            arrival_datetime = times_response - timedelta(minutes=10)
+            
+            # UNIXタイムスタンプ計算
+            jst_offset = timedelta(hours=9)  # JSTはUTC+9
+            arrival_datetime = arrival_datetime.replace(tzinfo=timezone.utc)  # UTCにセット
+            # arrival_datetime = arrival_datetime + jst_offset  # JSTに変換
+            unix_timestamp = int(arrival_datetime.timestamp())
+            
+            # 座標を取得
+            departure_location = gmaps.geocode(departure_point)
+            arrival_location = gmaps.geocode(arrival_point)
+            
+            departure_lat_lng = f"{departure_location[0]['geometry']['location']['lat']},{departure_location[0]['geometry']['location']['lng']}"
+            arrival_lat_lng = f"{arrival_location[0]['geometry']['location']['lat']},{arrival_location[0]['geometry']['location']['lng']}"
+            
+            # URL生成
+            google_maps_url = f"https://www.google.com/maps/dir/{departure_lat_lng}/{arrival_lat_lng}/am=t/data=!4m9!4m8!1m1!4e1!1m0!2m3!6e1!7e2!8j{unix_timestamp}!3e3?entry=ttu"
         
-        # times_responseをdatetimeオブジェクトに変換
-        # year, month, day, hour, minute, second = map(int, times_response.split(','))
-        arrival_datetime = times_response - timedelta(minutes=10)
+        # st.write(f"出発地点座標: {departure_lat_lng}")
+        # st.write(f"到着地点座標: {arrival_lat_lng}")
+        # st.write(f"UNIXタイムスタンプ: {unix_timestamp}")
+        st.write(f"Google Maps 乗換経路 URL: {google_maps_url}")
+        st.write(f"Google Calendar 登録 URL:\n {event_response}")
         
-        # UNIXタイムスタンプ計算
-        jst_offset = timedelta(hours=9)  # JSTはUTC+9
-        arrival_datetime = arrival_datetime.replace(tzinfo=timezone.utc)  # UTCにセット
-        # arrival_datetime = arrival_datetime + jst_offset  # JSTに変換
-        unix_timestamp = int(arrival_datetime.timestamp())
+        # departure_lat = departure_location[0]['geometry']['location']['lat']
+        # departure_lon = departure_location[0]['geometry']['location']['lng']
+        # arrival_lat = arrival_location[0]['geometry']['location']['lat']
+        # arrival_lon = arrival_location[0]['geometry']['location']['lng']
         
-        # 座標を取得
-        departure_location = gmaps.geocode(departure_point)
-        arrival_location = gmaps.geocode(arrival_point)
+        # map_data = pd.DataFrame({
+        # 'lat': [departure_lat, arrival_lat],
+        # 'lon': [departure_lon, arrival_lon]
+        # })
+        # st.map(map_data)
+
+with col2:
+    if st.button("カレンダー登録URLのみ生成"):
+        with st.spinner('生成処理中...'):
+            event_response = calendar_registration(event_info)  # 関数を呼び出し
+
+        st.write(f"Google Calendar 登録 URL:\n {event_response}")
         
-        departure_lat_lng = f"{departure_location[0]['geometry']['location']['lat']},{departure_location[0]['geometry']['location']['lng']}"
-        arrival_lat_lng = f"{arrival_location[0]['geometry']['location']['lat']},{arrival_location[0]['geometry']['location']['lng']}"
         
-        # URL生成
-        google_maps_url = f"https://www.google.com/maps/dir/{departure_lat_lng}/{arrival_lat_lng}/am=t/data=!4m9!4m8!1m1!4e1!1m0!2m3!6e1!7e2!8j{unix_timestamp}!3e3?entry=ttu"
-    
-    # st.write(f"出発地点座標: {departure_lat_lng}")
-    # st.write(f"到着地点座標: {arrival_lat_lng}")
-    # st.write(f"UNIXタイムスタンプ: {unix_timestamp}")
-    st.write(f"Google Maps 乗換経路 URL: {google_maps_url}")
-    st.write(f"Google Calendar 登録 URL:\n {event_response}")
-    
-    # departure_lat = departure_location[0]['geometry']['location']['lat']
-    # departure_lon = departure_location[0]['geometry']['location']['lng']
-    # arrival_lat = arrival_location[0]['geometry']['location']['lat']
-    # arrival_lon = arrival_location[0]['geometry']['location']['lng']
-    
-    # map_data = pd.DataFrame({
-    # 'lat': [departure_lat, arrival_lat],
-    # 'lon': [departure_lon, arrival_lon]
-    # })
-    # st.map(map_data)
-    
+        
 st.markdown("""
     <style>
         .footer {
