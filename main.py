14e7bbc6c324e7db952bea52acc533b3f4f72ada
Lemio <kota.shimizu.biz@gmail.com>
2023-09-28 09:46:07 +0900

    表記などを修正

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ from llm import extract_event_details
 load_dotenv()
 gmaps = googlemaps.Client(key=os.getenv('GOOGLE_MAPS_API_KEY'))
 
+st.set_page_config(
+    page_title="遅刻しなAI",  # Set the browser tab title to "My App"
+)
+
 st.title('遅刻しなAI')
 st.write("このアプリは予定通知メールなどの本文からコピペでカレンダー登録と予定地までの電車経路のURLを生成するアプリです。")
+st.markdown("**スマホの場合Chromeブラウザで開くとアプリ遷移がスムーズになるかもしれません**")
 
 departure_point = st.text_input("出発地点:", placeholder="住所または新宿駅など駅名を入力")
 # st.caption("住所や新宿駅など駅名を入力")
@@ -20,29 +25,29 @@ event_info = st.text_area("予定情報:", placeholder="スケジュール内容
 # st.caption("または日付けと目的地が入った文章を入力 → 10/15 18時から渋谷集合で")
 
 if st.button("URL生成"):
-    
-    place_response, times_response, event_response = extract_event_details(event_info)  # 関数を呼び出し
-    arrival_point = place_response  # 到着地点にplace_responseを格納
-    
-    # times_responseをdatetimeオブジェクトに変換
-    year, month, day, hour, minute, second = map(int, times_response.split(','))
-    arrival_datetime = datetime(year, month, day, hour, minute, second) - timedelta(minutes=10)
-    
-    # UNIXタイムスタンプ計算
-    jst_offset = timedelta(hours=9)  # JSTはUTC+9
-    arrival_datetime = arrival_datetime.replace(tzinfo=timezone.utc)  # UTCにセット
-    # arrival_datetime = arrival_datetime + jst_offset  # JSTに変換
-    unix_timestamp = int(arrival_datetime.timestamp())
-    
-    # 座標を取得
-    departure_location = gmaps.geocode(departure_point)
-    arrival_location = gmaps.geocode(arrival_point)
-    
-    departure_lat_lng = f"{departure_location[0]['geometry']['location']['lat']},{departure_location[0]['geometry']['location']['lng']}"
-    arrival_lat_lng = f"{arrival_location[0]['geometry']['location']['lat']},{arrival_location[0]['geometry']['location']['lng']}"
-    
-    # URL生成
-    google_maps_url = f"https://www.google.com/maps/dir/{departure_lat_lng}/{arrival_lat_lng}/am=t/data=!4m9!4m8!1m1!4e1!1m0!2m3!6e1!7e2!8j{unix_timestamp}!3e3?entry=ttu"
+    with st.spinner('生成処理中...'):
+        place_response, times_response, event_response = extract_event_details(event_info)  # 関数を呼び出し
+        arrival_point = place_response  # 到着地点にplace_responseを格納
+        
+        # times_responseをdatetimeオブジェクトに変換
+        year, month, day, hour, minute, second = map(int, times_response.split(','))
+        arrival_datetime = datetime(year, month, day, hour, minute, second) - timedelta(minutes=10)
+        
+        # UNIXタイムスタンプ計算
+        jst_offset = timedelta(hours=9)  # JSTはUTC+9
+        arrival_datetime = arrival_datetime.replace(tzinfo=timezone.utc)  # UTCにセット
+        # arrival_datetime = arrival_datetime + jst_offset  # JSTに変換
+        unix_timestamp = int(arrival_datetime.timestamp())
+        
+        # 座標を取得
+        departure_location = gmaps.geocode(departure_point)
+        arrival_location = gmaps.geocode(arrival_point)
+        
+        departure_lat_lng = f"{departure_location[0]['geometry']['location']['lat']},{departure_location[0]['geometry']['location']['lng']}"
+        arrival_lat_lng = f"{arrival_location[0]['geometry']['location']['lat']},{arrival_location[0]['geometry']['location']['lng']}"
+        
+        # URL生成
+        google_maps_url = f"https://www.google.com/maps/dir/{departure_lat_lng}/{arrival_lat_lng}/am=t/data=!4m9!4m8!1m1!4e1!1m0!2m3!6e1!7e2!8j{unix_timestamp}!3e3?entry=ttu"
     
     # st.write(f"出発地点座標: {departure_lat_lng}")
     # st.write(f"到着地点座標: {arrival_lat_lng}")
@@ -60,4 +65,21 @@ if st.button("URL生成"):
     # 'lon': [departure_lon, arrival_lon]
     # })
     # st.map(map_data)
+    
+st.markdown("""
+    <style>
+        .footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: #f1f1f1;
+            text-align: center;
+            padding: 10px;
+        }
+    </style>
+    <div class="footer">
+        <p>問い合わせはこちらまで: <a href="https://twitter.com/lemilemilemio" target="_blank">https://twitter.com/lemilemilemio</a></p>
+    </div>
+    """, unsafe_allow_html=True)
     
